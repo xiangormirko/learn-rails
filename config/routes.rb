67747d5cbbf649a7191b8e6e58d6717942538952
xiangormirko <xiangormirko@gmail.com>
2017-10-15 23:32:09 +0800

    contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
+  resources :contacts, only: [:new, :create]
   root to: 'visitors#new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
