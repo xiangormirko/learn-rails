9fa5179ffe3afa90981da13c00e2f71f27c17c1f
xiangormirko <xiangormirko@gmail.com>
2017-11-02 21:35:20 +0800

    mailing list

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,5 +4,6 @@ class UserMailer < ApplicationMailer
   def contact_email(contact)
     @contact = contact
     mail(to: Rails.application.secrets.owner_email, from: @contact.email, :subject => "Website Contact")
+    print "sending email"
   end
 end
